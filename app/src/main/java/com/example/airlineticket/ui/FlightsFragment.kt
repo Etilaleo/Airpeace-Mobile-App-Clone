4cc3768773f74affff86fb06afb97781ff24637c
leo <108675763+Etilaleo@users.noreply.github.com>
2023-05-06 00:07:05 +0100

    updated

--- app/src/main/java/com/example/airlineticket/ui/FlightsFragment.kt
+++ app/src/main/java/com/example/airlineticket/ui/FlightsFragment.kt
@@ -1,11 +1,15 @@
 package com.example.airlineticket.ui
 
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.airlineticket.R
 import com.example.airlineticket.databinding.FragmentFlightsBinding
 
 class FlightsFragment : Fragment() {
@@ -24,6 +28,11 @@ class FlightsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         actionBar()
 
+        binding.adultText.setOnClickListener {
+            dialog("1")
+        }
+
+
         binding.oneWay.setOnClickListener {
             binding.returnDate.visibility = View.GONE
         }
@@ -31,6 +40,30 @@ class FlightsFragment : Fragment() {
             binding.returnDate.visibility = View.VISIBLE
         }
 
+    }
+
+    private fun dialog(num : String) {
+        val builder = AlertDialog.Builder(requireContext()).apply {
+            this.setCancelable(true)
+            val customLayout = layoutInflater.inflate(R.layout.custom_numberpicker_dialog, null)
+            this.setView(customLayout)
+            val np = customLayout.findViewById<NumberPicker>(R.id.numberPicker)
+            np.maxValue = 10
+            np.minValue = 1
+            np.wrapSelectorWheel = true
+            np.setOnValueChangedListener { _, _, newVal ->
+                if (num == "1") {
+                    val aText = "$newVal Adult,"
+                    binding.adultText.text = newVal.toString()
+                    binding.howManyPassengersAdult.text = aText
+                }
+            }
+            this.setNegativeButton("OK") { _: DialogInterface, _: Int ->
+
+            }
+
+        }.create()
+        builder.show()
     }
 
     //Customizing our Appbar
